74083473eced604012cbf3d7e6e57854d87594af
Feras <feras.salim@climaxmedia.com>
2014-05-21 18:07:59 -0400

    Add gopm support

--- youtube-dl-go/src/main.go
+++ youtube-dl-go/src/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"flag"
+	"net/url"
+)
+
+var cmd = flag.String("command", "youtube-dl", "The command for the youtube-dl executable including the full path if applicable")
 
 func main() {
-	fmt.Printf("Hello world!")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	url := url.QueryEscape(r.FormValue("url"))
+	fmt.Fprintf(w, "Hi there, I love %s!", url)
 }
